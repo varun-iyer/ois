d32f9087d5b917587af06c9dee8431b5b9306ac5
Varun Iyer <varun_iyer@posteo.net>
2020-08-13 14:05:24 -0700

    CUDA Kernel works!

--- nvlib/device.h
+++ nvlib/device.h
@@ -1,6 +1,6 @@
 #ifndef __DEVICE_H__
 #define __DEVICE_H__
 extern __shared__ double sm[];
-__global__ void par_reduce(double *d, double *c1, double *c2);
-__global__ void par_reduce_mask(double *d, double *c1, double *c2, char *mask);
+__global__ void par_reduce(size_t n, double *d, double *c1, double *c2);
+__global__ void par_reduce_mask(size_t n, double *d, double *c1, double *c2, char *mask);
 #endif /* __DEVICE_H__ */
--- nvlib/nvmas.cu
+++ nvlib/nvmas.cu
@@ -1,16 +1,11 @@
+#include <stdio.h>
 #include "device.h"
 #include "nvmas.h"
 
+#define MAX_THREADS 1024
+#define SMSIZE (sizeof(double) * MAX_THREADS)
+
 extern "C" double multiply_and_sum(size_t nsize, double* h_C1, double* h_C2) {
-	size_t size = nsize * sizeof(double);
-
-	int threads = nsize;
-	int blocks = nsize / threads; // number of blocks
-	dim3 dimBlock(threads, 1, 1);
-	dim3 dimGrid(blocks, 1, 1);
-
-	int smSize = threads * sizeof(int);
-
 	double *d_C1, *d_C2, *d;
 
 	cudaMalloc(&d_C1, nsize * sizeof(double));
@@ -20,38 +15,20 @@ extern "C" double multiply_and_sum(size_t nsize, double* h_C1, double* h_C2) {
 	cudaMemcpy(d_C1, h_C1, nsize * sizeof(double), cudaMemcpyHostToDevice);
 	cudaMemcpy(d_C2, h_C2, nsize * sizeof(double), cudaMemcpyHostToDevice);
 
-	par_reduce<<<dimGrid, dimBlock, smSize>>>(d, d_C1, d_C2);
-
-	uint todo;
-	if (blocks > 1) todo = 1 + blocks/128;
-	else todo = 0;
-
-	for(int i = 0; i < todo; i++) {
-		threads = (blocks < nsize) ? blocks : nsize;
-		blocks = blocks / threads;
-		dim3 dimBlock(threads, 1, 1);
-		dim3 dimGrid(blocks, 1, 1);
-		par_reduce<<<dimGrid, dimBlock, smSize>>>(d, d_C1, d_C2); 
-	}
+	dim3 dimBlock(MAX_THREADS, 1, 1);
+	dim3 dimGrid(nsize / MAX_THREADS + 1, 1, 1);
+	par_reduce<<<dimGrid, dimBlock, SMSIZE>>>(nsize, d, d_C1, d_C2);
 
 	double h_r;
 	cudaMemcpy(&h_r, d, sizeof(double), cudaMemcpyDeviceToHost);
-
 	cudaFree(d_C1);
 	cudaFree(d_C2);
+	cudaFree(d);
+
     return h_r;
 }
 
 extern "C" double multiply_and_sum_mask(size_t nsize, double* h_C1, double* h_C2, char *h_m) {
-	size_t size = nsize * sizeof(double);
-
-	int threads = nsize;
-	int blocks = nsize / threads; // number of blocks
-	dim3 dimBlock(threads, 1, 1);
-	dim3 dimGrid(blocks, 1, 1);
-
-	int smSize = threads * sizeof(int);
-
 	double *d_C1, *d_C2, *d;
 	char *d_m;
 
@@ -64,24 +41,15 @@ extern "C" double multiply_and_sum_mask(size_t nsize, double* h_C1, double* h_C2
 	cudaMemcpy(d_C2, h_C2, nsize * sizeof(double), cudaMemcpyHostToDevice);
 	cudaMemcpy(d_m, h_m, nsize * sizeof(char), cudaMemcpyHostToDevice);
 
-	par_reduce_mask<<<dimGrid, dimBlock, smSize>>>(d, d_C1, d_C2, d_m);
-
-	uint todo;
-	if (blocks > 1) todo = 1 + blocks/128;
-	else todo = 0;
-
-	for(int i = 0; i < todo; i++) {
-		threads = (blocks < nsize) ? blocks : nsize;
-		blocks = blocks / threads;
-		dim3 dimBlock(threads, 1, 1);
-		dim3 dimGrid(blocks, 1, 1);
-		par_reduce_mask<<<dimGrid, dimBlock, smSize>>>(d, d_C1, d_C2, d_m); 
-	}
+	dim3 dimBlock(MAX_THREADS, 1, 1);
+	dim3 dimGrid(nsize / MAX_THREADS + 1, 1, 1);
+	par_reduce_mask<<<dimGrid, dimBlock, SMSIZE>>>(nsize, d, d_C1, d_C2, d_m);
 
 	double h_r;
 	cudaMemcpy(&h_r, d, sizeof(double), cudaMemcpyDeviceToHost);
 
 	cudaFree(d_C1);
 	cudaFree(d_C2);
+	cudaFree(d);
     return h_r;
 }
